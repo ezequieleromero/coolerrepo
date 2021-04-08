289cf52e1423673375ebe7a023cd5a3a396353fe
Ezequiel Romero <ezequieleromero@gmail.com>
2021-04-07 18:57:09 -0700

    v1

---
+++ Assets/Editor/PlayMode/ButtonTest.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    
+    public class ButtonTest
+    {
+        bool clicked;
+        float sec = 5;
+        private GameObject pauseCanvas = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/PauseMenu.prefab");
+        private GameObject pauseCanvasInst;
+
+        public static bool helpBool;
+
+        GameObject[] pauseObjects;
+        GameObject[] helpObjects;
+
+        [OneTimeSetUp]
+        public void LoadPauseMenu()
+        {
+            //pauseCanvasInst = Object.Instantiate(pauseCanvas, new Vector3(0, 0, 0), Quaternion.identity);
+
+            SceneManager.LoadScene("VetClinic");
+
+           /* pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+            pauseCanvasInst.PauseGameObject.hidePaused();
+            helpObjects = GameObject.FindGameObjectsWithTag("Help");
+            pauseCanvasInst.PauseGameObject.hideHelp();*/
+        }
+
+        /*// A Test behaves as an ordinary method
+        [Test]
+        public void ButtonTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }*/
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ButtonTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            //yield return PlayModeSetup();
+            yield return null;
+
+            //Assert PauseMenu closed
+            var pauseMenu = GameObject.Find("PauseMenu");
+            Assert.Null(pauseMenu);
+
+
+
+            /*pauseTopRightButton.onClick.AddListener(Clicked);
+            pauseTopRightButton.onClick.Invoke();
+
+            Assert.True(clicked);
+            clicked = false;
+
+            var resumeGO = GameObject.Find("Play Button");
+            var resumeButton = resumeGO.GetComponent<Button>();
+
+            Assert.NotNull(resumeButton);
+
+            resumeButton.onClick.AddListener(Clicked);
+            resumeButton.onClick.Invoke();
+
+            Debug.Log("Success");
+
+            Assert.True(clicked);
+            clicked = false;*/
+
+            //Loop TopRight Pause Button
+            //for (int i = 0; i < 1000; i++)
+            int i = 0;
+            while(true)
+            {
+                i++;
+                //Assert TopRight exists
+                var pauseTopRightGO = GameObject.Find("PauseTopRight");
+                var pauseTopRightButton = pauseTopRightGO.GetComponent<Button>();
+                Assert.NotNull(pauseTopRightButton);
+
+                clicked = false;
+
+                pauseTopRightButton.onClick.AddListener(Clicked);
+                pauseTopRightButton.onClick.Invoke();
+
+                Assert.True(clicked);
+                clicked = false;
+
+                var restartGO = GameObject.Find("Restart");
+                var restartButton = restartGO.GetComponent<Button>();
+
+                Assert.NotNull(restartButton);
+
+                restartButton.onClick.AddListener(Clicked);
+                restartButton.onClick.Invoke();
+
+                //Debug.Log("Success");
+
+                Assert.True(clicked);
+                clicked = false;
+                Debug.Log(i);
+
+                //yield return null;
+                //yield return new WaitForSeconds(sec);
+                //sec = sec / 2;
+                //Debug.Log("Sec: " + sec);
+            }
+            
+            
+
+            /*pauseMenu = GameObject.Find("PauseMenu");
+            Assert.NotNull(pauseMenu);*/
+        }
+
+        private void Clicked()
+        {
+            clicked = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool helpBool;
+
+    GameObject[] pauseObjects;
+    GameObject[] helpObjects;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+        helpObjects = GameObject.FindGameObjectsWithTag("Help");
+        hideHelp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseControl();
+        }
+        if (Input.GetKey("escape"))
+        {
+            Debug.Log("escaped");
+            Application.Quit();
+        }
+    }
+
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void hidePaused()
+    {
+        if (pauseObjects == null)
+            Debug.Log("pauseObjects is null");
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void pauseControl()
+    {
+        //Debug.Log("test");
+        if (Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    public void toggleHelp()
+    {
+        if (helpBool)
+        {
+            hideHelp();
+        }
+        else
+        {
+            showHelp();
+        }
+    }
+
+    public void showHelp()
+    {
+        foreach (GameObject g in helpObjects)
+        {
+            g.SetActive(true);
+        }
+        helpBool = true;
+    }
+
+    public void hideHelp()
+    {
+        foreach (GameObject g in helpObjects)
+        {
+            g.SetActive(false);
+        }
+        helpBool = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
